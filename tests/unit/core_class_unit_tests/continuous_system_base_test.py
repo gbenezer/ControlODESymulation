7378e5ba8ed5a1edbe8dec0d1ff4acf3f40fd73b
Gil Benezer <gil.benezer@gmail.com>
2025-12-30 23:38:12 -0500

    added the proper header

--- tests/unit/core_class_unit_tests/continuous_system_base_test.py
+++ tests/unit/core_class_unit_tests/continuous_system_base_test.py
@@ -1,7 +1,17 @@
 # Copyright (C) 2025 Gil Benezer
 #
-# COMPREHENSIVE Unit Tests for ContinuousSystemBase
-# Place at: tests/unit/core_class_unit_tests/continuous_system_base_test.py
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import unittest
 from typing import Optional
